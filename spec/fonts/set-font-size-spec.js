736a966bdce11108c24f38d78bd10900da51ac09
Silvestre Herrera <silvestre.herrera@gmail.com>
2017-04-13 15:41:59 -0300

    Add support for atom-dock. Remove tree-view settings as they're no longer necessary

--- spec/fonts/set-font-size-spec.js
+++ spec/fonts/set-font-size-spec.js
@@ -3,11 +3,18 @@
 import setFontSize from '../../lib/fonts/set-font-size';
 
 describe('Font size setter', () => {
-    it('should be able to change root element\'s font-size', () => {
-        const root = document.documentElement;
+    const root = document.documentElement;
 
+    it('should be able to change root element\'s font-size', () => {
         expect(root.style.fontSize).toBe('');
         setFontSize(22);
         expect(root.style.fontSize).toBe('22px');
+    });
+
+    it('should be able to unset root element\'s font-size', () => {
+        setFontSize(22);
+        expect(root.style.fontSize).toBe('22px');
+        setFontSize(null);
+        expect(root.style.fontSize).toBe('');
     });
 });
